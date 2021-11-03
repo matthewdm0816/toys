59473c5273fe72dd07dff6447cc15d88536233b7
KazamiMichiru <matthewdm0816@gmail.com>
2021-11-03 16:43:55 +0800

    12

--- train_coatnet.py
+++ train_coatnet.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 from torchvision.datasets import ImageNet
 import os.path as osp
-import torchvision.transforms as T
+import torchvision.transforms as transforms
+import pretty_errors
 from icecream import ic
 from coatnet import coatnet_0, coatnet_1, coatnet_2, coatnet_3, coatnet_4, count_parameters
 
@@ -10,12 +11,18 @@ dataset_root = osp.expanduser("~/data/imagenet1k")
 epochs = 100
 
 # ImageNet standard transform
-transform = torch.nn.Sequential(
-    T.ToTensor(),
-    T.Resize([224, 224]),
-    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-)
-transform = torch.jit.script(transform)
+# transform = torch.nn.Sequential(
+#     T.ToTensor(),
+#     T.Resize([224, 224]),
+#     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+# )
+# transform = torch.jit.script(transform)
+transform = transforms.Compose([
+    transforms.Resize([224, 224]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
 
 # Define datasets and dataloaders
 datasets = {
